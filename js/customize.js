18b98ffe4e5f0253179cac37308e928ef51e9920
rasheduzzaman shimanto <shovons26@gmail.com>
2017-07-18 02:17:21 +0600

    Add files via upload

---
+++ js/customize.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+
+    autoPlay: 3000, //Set AutoPlay to 3 seconds
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+});
+
+$(document).ready(function() {
+ 
+  $("#owl_demo_2").owlCarousel({
+
+    autoPlay: 3000, //Set AutoPlay to 3 seconds
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+});
